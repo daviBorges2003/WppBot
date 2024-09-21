347a493b615d42c5b657da7c48c8f27374bbd724
Davi Borges <davi.paula@hotmail.com>
2024-09-21 14:12:39 -0300

    Adicionei todos os arquivos

---
+++ BotWpp/Form1.cs
@@ -0,0 +1,18 @@
+namespace BotWpp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WppSendMessage w = new WppSendMessage();
+
+            w.SendMessage("Hello Word , Rebeca ", "@Rebeca");
+        
+        }
+    }
+}
---
+++ BotWpp/Message.cs
@@ -0,0 +1,21 @@
+﻿using EasyAutomationFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotWpp
+{
+    public class WppSendMessage : Web
+    {
+        public void SendMessage(string message , string to)
+        {
+            StartBrowser(TypeDriver.GoogleChorme , "C:\\Users\\davi.borges\\AppData\\Local\\Google\\Chrome\\User Data");
+
+            Navigate("https://web.whatsapp.com/");
+
+            WaitForLoad(); 
+        }
+    }
+}
